b08e674de2372cfa65bd0f07eeccad5c7427c8a3
Henrick <ricknogueira1231@gmail.com>
2023-11-27 23:10:01 -0300

    Aula 2 40 min

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,16 +1,51 @@
+import 'package:app_filmes/modules/login/login_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:get/get.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends GetView<LoginController> {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
-      body: const Stack(
-        children: [],
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Image.asset(
+            'assets/images/background.png',
+            width: Get.width,
+            height: Get.height,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            color: Colors.black45,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 80.0),
+            child: Column(
+              children: [
+                Image.asset('assets/images/logo.png'),
+                const SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                  width: Get.width * .9,
+                  height: 42,
+                  child: SignInButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(180),
+                    ),
+                    Buttons.Google,
+                    text: 'Login com o google',
+                    onPressed: () => controller.login(),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
