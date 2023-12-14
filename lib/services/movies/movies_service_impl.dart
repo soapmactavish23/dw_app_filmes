8c2713ca431c412ad1f19375f8594875d8b20070
Henrick <ricknogueira1231@gmail.com>
2023-12-14 00:06:28 -0300

    Aula 05 - 33:56

---
+++ lib/services/movies/movies_service_impl.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:app_filmes/models/movie_model.dart';
+import 'package:app_filmes/repositories/movies/movies_repository.dart';
+import 'package:app_filmes/services/movies/movies_service.dart';
+
+class MoviesServiceImpl implements MoviesService {
+  final MoviesRepository _moviesRepository;
+
+  MoviesServiceImpl({
+    required MoviesRepository moviesRepository,
+  }) : _moviesRepository = moviesRepository;
+
+  @override
+  Future<List<MovieModel>> getPopularMovies() =>
+      _moviesRepository.getPopularMovies();
+
+  @override
+  Future<List<MovieModel>> getTopRated() => _moviesRepository.getTopRated();
+}
